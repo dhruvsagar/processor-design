00f05dac601a183810a510e54b6753072c44ebd2
rodrims <rodri.m.sanchez@gmail.com>
2017-02-17 03:56:42 -0500

    Made minor changes to assignment1_frame.v, mostly formatting

--- Assignment 1/assignment1_frame_restored/assignment1_frame.v
+++ Assignment 1/assignment1_frame_restored/assignment1_frame.v
@@ -13,8 +13,6 @@ module assignment1_frame(
 	output [9:0] LEDR
 );
 
-	
-	
 	wire clk;
 	wire rst;
 
@@ -28,47 +26,45 @@ module assignment1_frame(
 	reg [9:0] red_lights = 0;
 	reg [31:0] count = 1;
 	reg [31:0] blink_reg = 32'd25000000;
+	reg [3:0] debug = 2;
 
 	reg key0_reg = 0;
 	reg key1_reg = 0;
-	
-	
+
 	assign clk = CLOCK_50;
 	assign rst = ~RESET_N;
 
-
 	assign key0_triggered = ~KEY[0] && ~key0_reg;
 	assign key1_triggered = ~KEY[1] && ~key1_reg;
-	
-	// key0_triggered and key1_triggered signales works like an edge triggered signal 
-		
-	
 
-	
+	// key0_triggered and key1_triggered signals works like an edge triggered signal 
+
 	always@(posedge clk or posedge rst)
 	begin
-		if(rst) begin
+		if (rst) begin
 			key0_reg <= 1'b0;
 			key1_reg <= 1'b0;
 			blink_reg <= 32'd25000000;
 			state_number <= 2;
-		end
-		else begin
+		end else begin
 			key0_reg <= ~KEY[0];
 			key1_reg <= ~KEY[1];
-			if (key0_triggered)
+			if (key0_triggered) begin
 				if (blink_reg < 100000000) begin
 					blink_reg <= blink_reg + 12500000;
 					state_number <= state_number + 1;
 				end
-			else if (key1_triggered)
+			end
+			
+			if (key1_triggered) begin
 				if (blink_reg > 12500000) begin
 					blink_reg <= blink_reg - 12500000;
 					state_number <= state_number - 1;
 				end
+			end
 		end
 	end
-	   
+  
 	/*always@(posedge clk or posedge rst)
 	begin: KEY1_REG
 		if(rst) begin
@@ -84,24 +80,24 @@ module assignment1_frame(
 				end
 		end
 	end*/
-	
-						
-	always@(posedge clk or posedge rst)begin 
+					
+	always@(posedge clk or posedge rst)begin
+		debug <= blink_reg / 32'd12500000;
 		if (rst) begin
 			red_lights <= 0;
 			count <= 1;
 			current_state <= 1;
 			state <= 1;
-		end
-		else if (count == blink_reg) begin
+		end else if (count == blink_reg) begin
 			count <= 1;
 			if (current_state > 6) begin
+				current_state <= 1;
 				if (state == 3) begin 
 					state <= 1;
 					red_lights <= 0;
+				end else begin
+					state <= state + 1;
 				end
-				else state <= state + 1;
-				current_state <= 1;
 			end
 			
 			if (current_state <= 6) begin
@@ -109,46 +105,45 @@ module assignment1_frame(
 					red_lights <= red_lights ^ 10'b1111100000;
 					current_state <= current_state + 1;
 				end
+				
 				if (state == 2) begin
 					red_lights <= red_lights ^ 10'b0000011111;
 					current_state <= current_state + 1;
 				end
+				
 				if (state == 3) begin
-					if (current_state == 1)
+					if (current_state == 1) begin
 						red_lights <= 10'b1111100000;
-					else 
+					end else begin 
 						red_lights <= red_lights ^ 10'b1111111111;
+					end
 					current_state <= current_state + 1;
 				end
 			end
-		end
-		else count <= count + 1;
-				
-			
+		end else begin
+			count <= count + 1;
+		end	
 	end
-
+	  
+	assign LEDR[0]= red_lights[0];
+	assign LEDR[1]= red_lights[1];
+	assign LEDR[2]= red_lights[2];
+	assign LEDR[3]= red_lights[3];
+	assign LEDR[4]= red_lights[4];
+	assign LEDR[5]= red_lights[5];
+	assign LEDR[6]= red_lights[6];
+	assign LEDR[7]= red_lights[7];
+	assign LEDR[8]= red_lights[8];
+	assign LEDR[9]= red_lights[9];
 	
+	SevenSeg ss0(.IN(state_number),.OFF(1'b0),.OUT(HEX0));
 		
 	/* For debugging */
-	  assign LEDR[0]= red_lights[0];
-	  assign LEDR[1]= red_lights[1];
-	  assign LEDR[2]= red_lights[2];
-	  assign LEDR[3]= red_lights[3];
-	  assign LEDR[4]= red_lights[4];
-	  assign LEDR[5]= red_lights[5];
-	  assign LEDR[6]= red_lights[6];
-	  assign LEDR[7]= red_lights[7];
-	  assign LEDR[8]= red_lights[8];
-	  assign LEDR[9]= red_lights[9];
-	
-	SevenSeg ss0(.IN(state_number),.OFF(1'b0),.OUT(HEX3));
-	/*
-	SevenSeg ss1(.IN(timer[31:28]),.OFF(1'b0),.OUT(HEX1));
-	SevenSeg ss2(.IN(timer[27:24]),.OFF(1'b0),.OUT(HEX2));
-	SevenSeg ss3(.IN(timer[23:20]),.OFF(1'b0),.OUT(HEX3));
-	SevenSeg ss4(.IN(timer[19:16]),.OFF(1'b0),.OUT(HEX4));
-	SevenSeg ss5(.IN(blink_reg),.OFF(1'b0),.OUT(HEX5));
-	*/
 
-	
+	SevenSeg ss1(.IN(state_number),.OFF(1'b1),.OUT(HEX1));
+	SevenSeg ss2(.IN(state_number),.OFF(1'b1),.OUT(HEX2));
+	SevenSeg ss3(.IN(state_number),.OFF(1'b1),.OUT(HEX3));
+	SevenSeg ss4(.IN(state_number),.OFF(1'b1),.OUT(HEX4));
+	SevenSeg ss5(.IN(debug),.OFF(1'b1),.OUT(HEX5));
+
 endmodule
